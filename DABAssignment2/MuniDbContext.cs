0fe6a0d3d06a886544f47accec8a58a23d5801fd
magnusblaa <magnus.moeller@hotmail.com>
2022-04-01 10:43:54 +0200

    added nugets

---
+++ DABAssignment2/MuniDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DABAssignment2
+{
+    public class MuniDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "TrustServerCertificate=true; Data Source=localhost; Initial Catalog=??; Persist Security Info=True; User ID=sa;Password=<??>;");
+        }
+    }
+}
