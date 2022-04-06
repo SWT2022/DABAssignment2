7660ce9f52b46e5b6f15cdde215c08b42516b6cd
Jacob Gade <jacobcgade@gmail.com>
2022-04-06 13:07:50 +0200

    Small changes as rooms now is a table itself

--- DABAssignment2/QueryHelpers/QueryHelper.cs
+++ DABAssignment2/QueryHelpers/QueryHelper.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DABAssignment2.QueryHelpers
 {
     public class QueryHelper
     {
-        public void ListRoomsNAddresses(MuniDbContext context)
+        public async void ListRoomsNAddresses(MuniDbContext context)
         {
-            var rooms = context.Rooms.ToList();
+            var rooms = await context.Rooms.ToListAsync();
             foreach (var room in rooms)
             {
-                Console.WriteLine($"Name: {room.Name} Address: {room.Address}");
+                Console.WriteLine($"Name: {room.RoomId} Address: {room.Location.Address}");
             }
         }
 
