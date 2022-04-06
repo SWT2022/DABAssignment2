8863411950cf9fd61071c42876ec624cda2b2b8b
Jacob Gade <jacobcgade@gmail.com>
2022-04-06 13:05:22 +0200

    Work started on Program.cs and helper functions to query DB

---
+++ DABAssignment2/QueryHelpers/QueryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DABAssignment2.QueryHelpers
+{
+    public class QueryHelper
+    {
+        public void ListRoomsNAddresses(MuniDbContext context)
+        {
+            var rooms = context.Rooms.ToList();
+            foreach (var room in rooms)
+            {
+                Console.WriteLine($"Name: {room.Name} Address: {room.Address}");
+            }
+        }
+
+        public void ListSocietiesByActivity(MuniDbContext context)
+        {
+
+        }
+
+        public void ListBookings(MuniDbContext context)
+        {
+
+        }
+    }
+}
